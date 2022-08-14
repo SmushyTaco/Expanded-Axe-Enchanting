8cb8a00808df95f3fcf8b3f566c649014fb0cfa1
SmushyTaco <smushytaco@gmail.com>
2022-08-14 04:16:32 -0700

    1.19.2

--- Expanded Axe Enchanting/src/main/java/com/smushytaco/expanded_axe_enchanting/mixins/AxeModification.java
+++ Expanded Axe Enchanting/src/main/java/com/smushytaco/expanded_axe_enchanting/mixins/AxeModification.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Enchantment.class)
-@SuppressWarnings("ConstantConditions")
 public abstract class AxeModification {
     @Shadow
     @Final
