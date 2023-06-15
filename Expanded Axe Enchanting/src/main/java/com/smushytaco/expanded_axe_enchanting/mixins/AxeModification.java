0943f6fa7c41d6c63391024758104820922689bf
SmushyTaco <smushytaco@gmail.com>
2023-06-15 13:50:04 -0700

    1.20.1

--- Expanded Axe Enchanting/src/main/java/com/smushytaco/expanded_axe_enchanting/mixins/AxeModification.java
+++ Expanded Axe Enchanting/src/main/java/com/smushytaco/expanded_axe_enchanting/mixins/AxeModification.java
@@ -1,4 +1,5 @@
 package com.smushytaco.expanded_axe_enchanting.mixins;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.smushytaco.expanded_axe_enchanting.ExpandedAxeEnchanting;
 import net.minecraft.enchantment.*;
 import net.minecraft.item.AxeItem;
@@ -7,25 +8,24 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Enchantment.class)
 public abstract class AxeModification {
     @Shadow
     @Final
     public EnchantmentTarget target;
-    @Inject(method = "isAcceptableItem", at = @At("HEAD"), cancellable = true)
-    public void isAcceptableItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+    @ModifyReturnValue(method = "isAcceptableItem", at = @At("RETURN"))
+    public boolean isAcceptableItem(boolean original, ItemStack stack) {
         Enchantment enchantment = (Enchantment) (Object) this;
         if (enchantment instanceof FireAspectEnchantment) {
-            if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseFireAspectOnAxe() || !(stack.getItem() instanceof AxeItem)) return;
-            cir.setReturnValue(true);
+            if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseFireAspectOnAxe() || !(stack.getItem() instanceof AxeItem)) return original;
+            return true;
         } else if (enchantment instanceof KnockbackEnchantment) {
-            if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseKnockbackOnAxe() || !(stack.getItem() instanceof AxeItem)) return;
-            cir.setReturnValue(true);
+            if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseKnockbackOnAxe() || !(stack.getItem() instanceof AxeItem)) return original;
+            return true;
         }  else if (enchantment instanceof LuckEnchantment) {
-            if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseLootingOnAxe() || target != EnchantmentTarget.WEAPON || !(stack.getItem() instanceof AxeItem)) return;
-            cir.setReturnValue(true);
+            if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseLootingOnAxe() || target != EnchantmentTarget.WEAPON || !(stack.getItem() instanceof AxeItem)) return original;
+            return true;
         }
+        return original;
     }
 }
