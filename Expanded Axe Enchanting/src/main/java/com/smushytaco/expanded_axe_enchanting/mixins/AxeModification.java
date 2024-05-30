6c79505f4feea9a6355944127245e71f0f8f0ecf
Nikan Radan <github.com@alias.nikanradan.com>
2024-05-30 16:31:18 -0700

    1.20.6

--- Expanded Axe Enchanting/src/main/java/com/smushytaco/expanded_axe_enchanting/mixins/AxeModification.java
+++ Expanded Axe Enchanting/src/main/java/com/smushytaco/expanded_axe_enchanting/mixins/AxeModification.java
@@ -4,6 +4,7 @@ import com.smushytaco.expanded_axe_enchanting.ExpandedAxeEnchanting;
 import net.minecraft.enchantment.*;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.ItemTags;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,18 +13,18 @@ import org.spongepowered.asm.mixin.injection.At;
 public abstract class AxeModification {
     @Shadow
     @Final
-    public EnchantmentTarget target;
+    private Enchantment.Properties properties;
     @ModifyReturnValue(method = "isAcceptableItem", at = @At("RETURN"))
     public boolean isAcceptableItem(boolean original, ItemStack stack) {
         Enchantment enchantment = (Enchantment) (Object) this;
-        if (enchantment instanceof FireAspectEnchantment) {
+        if (enchantment == Enchantments.FIRE_ASPECT) {
             if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseFireAspectOnAxe() || !(stack.getItem() instanceof AxeItem)) return original;
             return true;
-        } else if (enchantment instanceof KnockbackEnchantment) {
+        } else if (enchantment == Enchantments.KNOCKBACK) {
             if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseKnockbackOnAxe() || !(stack.getItem() instanceof AxeItem)) return original;
             return true;
         }  else if (enchantment instanceof LuckEnchantment) {
-            if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseLootingOnAxe() || target != EnchantmentTarget.WEAPON || !(stack.getItem() instanceof AxeItem)) return original;
+            if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseLootingOnAxe() || properties.supportedItems() != ItemTags.SWORD_ENCHANTABLE || !(stack.getItem() instanceof AxeItem)) return original;
             return true;
         }
         return original;
--- Expanded Axe Enchanting/src/main/kotlin/com/smushytaco/expanded_axe_enchanting/ExpandedAxeEnchantingPreLaunch.kt
+++
@@ -1,5 +0,0 @@
-package com.smushytaco.expanded_axe_enchanting
-import com.llamalad7.mixinextras.MixinExtrasBootstrap
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
-@Suppress("UNUSED")
-object ExpandedAxeEnchantingPreLaunch: PreLaunchEntrypoint { override fun onPreLaunch() { MixinExtrasBootstrap.init() } }
