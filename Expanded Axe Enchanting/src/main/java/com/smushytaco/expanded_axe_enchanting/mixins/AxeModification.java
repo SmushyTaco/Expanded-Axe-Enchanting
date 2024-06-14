98b31001bd6a21300c74b4e0ce8484f96af69080
Nikan Radan <personal@nikanradan.com>
2024-06-14 16:45:07 -0700

    1.21

--- Expanded Axe Enchanting/src/main/java/com/smushytaco/expanded_axe_enchanting/mixins/AxeModification.java
+++ Expanded Axe Enchanting/src/main/java/com/smushytaco/expanded_axe_enchanting/mixins/AxeModification.java
@@ -4,27 +4,21 @@ import com.smushytaco.expanded_axe_enchanting.ExpandedAxeEnchanting;
 import net.minecraft.enchantment.*;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.tag.ItemTags;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 @Mixin(Enchantment.class)
 public abstract class AxeModification {
-    @Shadow
-    @Final
-    private Enchantment.Properties properties;
     @ModifyReturnValue(method = "isAcceptableItem", at = @At("RETURN"))
     public boolean isAcceptableItem(boolean original, ItemStack stack) {
         Enchantment enchantment = (Enchantment) (Object) this;
-        if (enchantment == Enchantments.FIRE_ASPECT) {
+        if (ExpandedAxeEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.FIRE_ASPECT)) {
             if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseFireAspectOnAxe() || !(stack.getItem() instanceof AxeItem)) return original;
             return true;
-        } else if (enchantment == Enchantments.KNOCKBACK) {
+        } else if (ExpandedAxeEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.KNOCKBACK)) {
             if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseKnockbackOnAxe() || !(stack.getItem() instanceof AxeItem)) return original;
             return true;
-        }  else if (enchantment instanceof LuckEnchantment) {
-            if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseLootingOnAxe() || properties.supportedItems() != ItemTags.SWORD_ENCHANTABLE || !(stack.getItem() instanceof AxeItem)) return original;
+        }  else if (ExpandedAxeEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.LOOTING)) {
+            if (!ExpandedAxeEnchanting.INSTANCE.getConfig().getCanUseLootingOnAxe() || !(stack.getItem() instanceof AxeItem)) return original;
             return true;
         }
         return original;
