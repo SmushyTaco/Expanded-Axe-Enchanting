371800619ab8c64fdb7c8bb2822e0d0ab801d329
Nikan Radan <personal@nikanradan.com>
2024-11-25 16:26:04 -0800

    New Config

---
+++ Expanded Axe Enchanting/src/main/kotlin/com/smushytaco/expanded_axe_enchanting/ModConfiguration.kt
@@ -0,0 +1,21 @@
+package com.smushytaco.expanded_axe_enchanting
+import io.wispforest.owo.config.annotation.*
+@Modmenu(modId = ExpandedAxeEnchanting.MOD_ID)
+@Config(name = ExpandedAxeEnchanting.MOD_ID, wrapperName = "ModConfig")
+@Suppress("UNUSED")
+class ModConfiguration {
+    @JvmField
+    var canUseFireAspectOnAxe = true
+    @JvmField
+    var canUseKnockbackOnAxe = true
+    @JvmField
+    var canUseLootingOnAxe = true
+    @JvmField
+    var canUseImpalingOnAxe = true
+    @JvmField
+    var canUseDensityOnAxe = true
+    @JvmField
+    var canUseBreachOnAxe = true
+    @JvmField
+    var canUseWindBurstOnAxe = true
+}
--- Expanded Axe Enchanting/src/main/kotlin/com/smushytaco/expanded_axe_enchanting/configuration_support/ModMenuIntegration.kt
+++
@@ -1,8 +0,0 @@
-package com.smushytaco.expanded_axe_enchanting.configuration_support
-import com.terraformersmc.modmenu.api.ConfigScreenFactory
-import com.terraformersmc.modmenu.api.ModMenuApi
-import me.shedaniel.autoconfig.AutoConfig
-class ModMenuIntegration: ModMenuApi {
-    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> =
-        ConfigScreenFactory { parent -> AutoConfig.getConfigScreen(ModConfiguration::class.java, parent).get() }
-}
