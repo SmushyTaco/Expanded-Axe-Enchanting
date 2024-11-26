10990447d95b24a9206177931c99302d207c152e
Nikan Radan <github.com@alias.nikanradan.com>
2024-11-25 16:26:04 -0800

    New Config

--- Expanded Axe Enchanting/src/main/kotlin/com/smushytaco/expanded_axe_enchanting/ExpandedAxeEnchanting.kt
+++ Expanded Axe Enchanting/src/main/kotlin/com/smushytaco/expanded_axe_enchanting/ExpandedAxeEnchanting.kt
@@ -1,8 +1,4 @@
 package com.smushytaco.expanded_axe_enchanting
-import com.smushytaco.expanded_axe_enchanting.configuration_support.ModConfiguration
-import me.shedaniel.autoconfig.AutoConfig
-import me.shedaniel.autoconfig.annotation.Config
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.minecraft.enchantment.Enchantment
@@ -12,14 +8,9 @@ import net.minecraft.registry.RegistryKeys
 import kotlin.jvm.optionals.getOrNull
 object ExpandedAxeEnchanting : ModInitializer {
     const val MOD_ID = "expanded_axe_enchanting"
-    lateinit var config: ModConfiguration
-        private set
+    val config = ModConfig.createAndLoad()
     private var dynamicRegistryManager: DynamicRegistryManager? = null
     override fun onInitialize() {
-        AutoConfig.register(ModConfiguration::class.java) { definition: Config, configClass: Class<ModConfiguration> ->
-            GsonConfigSerializer(definition, configClass)
-        }
-        config = AutoConfig.getConfigHolder(ModConfiguration::class.java).config
         ServerLifecycleEvents.SERVER_STARTED.register { server -> dynamicRegistryManager = server.registryManager }
     }
     fun isSameEnchantment(enchantment: Enchantment, enchantmentRegistryKey: RegistryKey<Enchantment>) = dynamicRegistryManager?.getOptional(RegistryKeys.ENCHANTMENT)?.getOrNull()?.getEntry(enchantment)?.matchesKey(enchantmentRegistryKey) ?: false
